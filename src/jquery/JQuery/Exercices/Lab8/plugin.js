9cd107542ad283d87e4f4137fabbf92f3fbc7a1a
Stephen Bechtold <sbechtold@buildinmotion.com>
2016-05-24 03:02:15 -0400

    Clean up

---
+++ src/jquery/JQuery/Exercices/Lab8/plugin.js
@@ -0,0 +1,10 @@
+/*
+    Should change the background color of the row in the event it has a population 
+    greater than 10,000,000
+    
+    Should accept an optional config to override the color 
+*/
+(function ($) { 
+    
+    
+})(jQuery) 
--- src/jquery/JQuery/Solutions/Lab8/plugin.js
+++ src/jquery/JQuery/Solutions/Lab8/plugin.js
@@ -1,4 +1,23 @@
+/*
+    Should change the background color of the row in the event it has a population 
+    greater than 10,000,000
+    
+    Should accept an optional config to override the color 
+*/
 (function ($) { 
-
+    
+    var defaultConfig = { color:"red" }
+    
+    $.fn.bigState = function(size, config) { 
+        var val = parseInt(size);
+        if (!config)
+            config = {};
+        
+        $.extend(true, config, defaultConfig);
+        console.log(size);
+        if (val && val >= 10000000)
+            this.css("background", config.color);
+      return this;  
+    };
     
 })(jQuery) 
